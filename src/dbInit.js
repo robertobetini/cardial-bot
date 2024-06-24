eb3d38269abd8dcd3c511f81ffa0955eef7026f7
Roberto Betini Junior <hissi.fantasma@gmail.com>
2024-06-24 02:13:50 -0300

    refactor: create a constant for sql script dir

--- src/dbInit.js
+++ src/dbInit.js
@@ -1,5 +1,7 @@
 const fs = require("fs");
 
+const SQL_SCRIPTS_DIR = "db_scripts/init";
+
 const dbName = process.env.DB_NAME;
 const db = require("better-sqlite3")(`${dbName}.db`);
 db.pragma('journal_mode = WAL');
@@ -10,9 +12,9 @@ module.exports = {
     init: () => {
         Logger.info(`Initializing sqlite3 database ${dbName}`);
         
-        const fileNames = fs.readdirSync("db_scripts/init");
+        const fileNames = fs.readdirSync(SQL_SCRIPTS_DIR);
         for (let fileName of fileNames) {
-            const tableCreationSql = fs.readFileSync(`db_scripts/init/${fileName}`, { encoding: "utf-8" });
+            const tableCreationSql = fs.readFileSync(`${SQL_SCRIPTS_DIR}/${fileName}`, { encoding: "utf-8" });
             db.prepare(tableCreationSql).run();
         }
         db.close();

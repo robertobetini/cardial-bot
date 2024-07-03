65fb22e0911ea019747b8eb740df0f6928e397ad
Roberto Betini Junior <hissi.fantasma@gmail.com>
2024-07-02 23:12:57 -0300

    chore: add table creation scripts

--- db_scripts/init/003-attributes_table_creation.sql
+++ db_scripts/init/003-attributes_table_creation.sql
@@ -2,12 +2,12 @@ CREATE TABLE IF NOT EXISTS ATTRIBUTES (
     userId VARCHAR(64) NOT NULL,
     guildId VARCHAR(64) NOT NULL, 
 
-    `FOR` INTEGER, 
-    DEX INTEGER, 
-    CON INTEGER, 
-    WIS INTEGER, 
-    CHA INTEGER,
-    availablePoints INTEGER,
+    `FOR` INT, 
+    DEX INT, 
+    CON INT, 
+    WIS INT, 
+    CHA INT,
+    availablePoints INT,
     firstAttributionDone BOOLEAN,
 
     FOREIGN KEY (userId, guildId) REFERENCES USERS(userId, guildId)

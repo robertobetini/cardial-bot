73a1a4b4827938c8df9704841cde5ea412480ae9
Roberto Betini Junior <hissi.fantasma@gmail.com>
2024-08-18 18:12:39 -0300

    refactor: update set role command to update multiple role types

--- src/models/role.js
+++ src/models/role.js
@@ -1,6 +1,16 @@
 class Role {
     static ADM_TYPE = "ADM";
-    static SILENT_TYPE = "SILENT";
+    static GM_TYPE = "GM";
+    static BATTLE_GM_TYPE = "BATTLE_GM";
+    static MODERATOR_TYPE = "MODERATOR";
+    static PLAYER_TYPE = "PLAYER";
+    static ENLIGHTENED_PLAYER_TYPE = "ENLIGHTENED_PLAYER";
+    static TEMP_PLAYER_TYPE = "TEMP_PLAYER";
+    static UNFINISHED_SHEET_TYPE = "UNFINISHED_SHEET";
+    static USER_TYPE = "USER";
+    static BOOSTER1_TYPE = "BOOSTER1";
+    static BOOSTER2_TYPE = "BOOSTER2";
+    static SILENT_TYPE = "SILENT"; // TODO: ver se vai apagar isso aqui ou n
 
     constructor(guildId, roleId, type) {
         this.guildId = guildId;

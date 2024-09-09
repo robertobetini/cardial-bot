478e7390a9a245839de01cfb39476b99f533b75c
Roberto Betini Junior <hissi.fantasma@gmail.com>
2024-09-08 22:47:37 -0300

    fix: infite money when transfering gold to themselve

--- src/commands/economy/transferGoldCommand.js
+++ src/commands/economy/transferGoldCommand.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
-const RoleService = require("./../../services/roleService");
 const EconomyService = require("../../services/economyService");
+const { SilentError } = require("../../errors/silentError");
 
 module.exports = {
     data: new Discord.SlashCommandBuilder()
@@ -20,6 +20,10 @@ module.exports = {
     async execute(interaction) {
         const target = interaction.options.getUser("user");
         const amount = interaction.options.getInteger("quantidade");
+
+        if (target.id === interaction.user.id) {
+            throw new SilentError("Você não pode transferir dinheiro para si mesmo .-.");
+        }
 
         EconomyService.transferGold(interaction.guild.id, interaction.user, target, amount);
 
